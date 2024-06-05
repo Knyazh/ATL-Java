dd2ca568293384f44212de2af1b06d6c3c73fea4
Knyaz <qnyazface@mail.ru>
2024-06-05 17:43:48 +0400

    Mirror effect done

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,23 @@
 public class Main {
     public static void main(String[] args) {
 
-//        for(int i = 0; i<7; i++){
-//            for(int j = 0; j<i; j++){
-//                System.out.print("*");
-//            }
-//            System.out.println();
-//        }
+        for(int i = 0; i<7; i++){
+            for(int j = 0; j<7-i; j++){
+                System.out.print(" ");
 
-
-            for(int i = 7 ; i>0; i--){
-                for(int j = 0 ; j < i ; j++){
-                    System.out.print("*");
-                }
-                System.out.println();
             }
+            for(int k = 0; k<i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//
+//            for(int i = 7 ; i>0; i--){
+//                for(int j = 0 ; j < i ; j++){
+//                    System.out.print("*");
+//                }
+//                System.out.println();
+
         }
     }
